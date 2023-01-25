0ce02a786e688585fbfc72355a99bd56a6badb73
fhhimel <fhhimel18@gmail.com>
2023-01-25 15:18:04 +0600

    first commit

---
+++ client/src/core/Copyright.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
+import Box from '@material-ui/core/Box';
+
+export default function Copyright() {
+  return (
+    <Box mt={8} mb={4}>
+      <Typography variant='body2' color='textSecondary' align='center'>
+        
+        <Link color='inherit' href='#'>
+          Fahim Hasan Himel : MERN Stack Developer (fhhimel@gmail.com)
+        </Link>{' '}
+        
+      </Typography>
+    </Box>
+  );
+}
